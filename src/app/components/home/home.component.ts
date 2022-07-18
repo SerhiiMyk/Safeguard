b69f1e4849a3d0d6962cc22c8516f107630882b9
SerhiiMyk <sergey.mykolaichuk@gmail.com>
2022-07-18 22:02:05 +0300

    edit home component (edit scroll function, implement AfterViewInit change header on scroll); edit mistake in phone error message

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,21 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
   showMobileMenu: boolean = false;
+
+  ngAfterViewInit() {
+    window.addEventListener('scroll', () => {
+      let header = document.querySelector('.header')
+      header?.classList.toggle('scrolling-active', window.scrollY > 0)
+    })
+  }
 
   scroll(direction: string) {
     if (this.showMobileMenu) {
-      this.showMobileMenu = !this.showMobileMenu
+      this.onBurgerMenuClick()
     }
     if (direction === 'up') {
       window.scrollTo({ top: 0, behavior: 'smooth' })
     } else {
-      window.scrollTo({ top: 850, behavior: 'smooth' })
+      window.scrollTo({ top: 860, behavior: 'smooth' })
     }
   }
 
